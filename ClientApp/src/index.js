18f9aa46f4483cbb3994d5e2ad6cbe2735e3231b
Joost Stam <joost.stam@gmail.com>
2018-10-03 20:45:27 +0200

    Demo app: build a countdown timer

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,19 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
+import App from './app';
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
-
-registerServiceWorker();
+    <App />,
+    document.getElementById('root')
+);

fc8fa21fcf1174949d9b5534c6dfeaced3a78b24
Joost Stam <joost.stam@gmail.com>
2018-10-23 17:14:38 +0200

    Redux Demo

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './components/app';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import './index.css'
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );

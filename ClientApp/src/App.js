3d197941ebb40f66f033d328378ebdb00742ecc2
Alexander <alexander_hockey@msn.com>
2018-10-31 15:21:53 +0100

    Added routing.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Home } from './components/Home';
 import { ProductPage } from './components/ProductPage';
+import { render } from 'react-dom';
+import {Route, BrowserRouter, Switch } from 'react-router-dom';
+// import Filterpage from './components/FilterPage';
 
 export default class App extends Component {
   displayName = App.name
 
   render() {
     return (
-      <div>
-        <ProductPage />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path={'/'} component={Home} exact />
+          <Route path={'/product'} component={ProductPage} />
+          {/* <Route path={'/filter'} component={Filterpage} /> */}
+        </Switch>
+       </BrowserRouter>
     );
   }
 }
--- ClientApp/src/components/SelectSize.js
+++ ClientApp/src/components/SelectSize.js
@@ -3,13 +3,13 @@ import { Button, Container } from 'semantic-ui-react'
 
 const SelectSize = () => 
     <Container>
-        <Button>XS</Button>
-        <Button>S</Button>
-        <Button disabled>M</Button>
-        <Button>L</Button>
-        <Button>XL</Button>
-        <Button>XXL</Button>
-        <Button disabled>3XL</Button>
+        <Button basic>XS</Button>
+        <Button basic>S</Button>
+        <Button disabled basic>M</Button>
+        <Button basic>L</Button>
+        <Button basic>XL</Button>
+        <Button basic>XXL</Button>
+        <Button disabled basic>3XL</Button>
     </Container>
 
 export default SelectSize

de0cdabe448bd5e83202a36051e403aba542f64d
Griffin33 <33390937+Griffin33@users.noreply.github.com>
2018-10-31 13:48:30 +0100

    Add sort button.

--- ClientApp/src/components/FilterPage.js
+++ ClientApp/src/components/FilterPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TopHeader from './Header';
 import FilterGrid from './FilterGrid';
+import FilterDropdown from './FilterDropdown';
 import { Container } from 'semantic-ui-react';
 import { Divider } from 'semantic-ui-react';
 
@@ -11,6 +12,9 @@ export class FilterPage extends Component {
     return ( 
       <div>
         <TopHeader />
+        <Container>
+          <FilterDropdown />
+        </Container>
         <Divider hidden />
         <Container>
           <FilterGrid />

001abbfbeb1e01451d3bae4f76bfa63dce533b01
Griffin33 <33390937+Griffin33@users.noreply.github.com>
2018-11-14 10:41:26 +0100

    Add breadcrumbs and changes to the filter page.

--- ClientApp/src/components/FilterPage.js
+++ ClientApp/src/components/FilterPage.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import TopHeader from './Header';
 import FilterGrid from './FilterGrid';
 import FilterDropdown from './FilterDropdown';
+import Breadcrumbs from './Breadcrumbs';
 import { Container } from 'semantic-ui-react';
-import { Divider } from 'semantic-ui-react';
+import {Grid, Segment, Divider } from 'semantic-ui-react';
 
 export class FilterPage extends Component {
   displayName = FilterPage.name
@@ -13,6 +14,8 @@ export class FilterPage extends Component {
       <div>
         <TopHeader />
         <Container>
+          <Breadcrumbs/>
+          <Divider hidden/>
           <FilterDropdown />
         </Container>
         <Divider hidden />
--- ClientApp/src/components/Grid2.js
+++ ClientApp/src/components/Grid2.js
@@ -1,53 +1,96 @@
-import React from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Grid, Image, Menu } from 'semantic-ui-react'
+import _ from 'lodash'
 
-const GridExampleColumCount = () => (
-<Grid>  
-  <Grid.Row columns={4} centered relaxed>
-    <Grid.Column>
-      <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-    </Grid.Column>
-    <Grid.Column>
-      <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-    </Grid.Column>
-    <Grid.Column>
-      <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-    </Grid.Column>
-    <Grid.Column>
-      <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-    </Grid.Column>
-  </Grid.Row>
+export default class Grid2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json
+        })
+      });
+  }
+  render() {
+    var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <Grid>
+        <Grid.Row columns={4} centered relaxed>
+          <Grid.Column>
+            {items.map(item => (
+              <Menu fluid vertical>
+                <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+                <Menu.Item className='header'>{item.name}</Menu.Item>
+              </Menu> 
+            ))}
+          </Grid.Column>
+        </Grid.Row>
 
-  <Grid.Row columns={4}>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-    </Grid.Row>
+        <Grid.Row columns={4}>
+          <Grid.Column>
+            <Menu fluid vertical>
+              <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              <Menu.Item className='header'>Cats</Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column>
+            <Menu fluid vertical>
+              <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              <Menu.Item className='header'>Cats</Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column>
+            <Menu fluid vertical>
+              <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              <Menu.Item className='header'>Cats</Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column>
+            <Menu fluid vertical>
+              <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              <Menu.Item className='header'>Cats</Menu.Item>
+            </Menu>
+          </Grid.Column>
+        </Grid.Row>
 
-    <Grid.Row columns={4}>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-)
-
-export default GridExampleColumCount
+        <Grid.Row columns={4}>
+          <Grid.Column>
+            <Menu fluid vertical>
+              <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              <Menu.Item className='header'>Cats</Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column>
+            <Menu fluid vertical>
+              <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              <Menu.Item className='header'>Cats</Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column>
+            <Menu fluid vertical>
+              <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              <Menu.Item className='header'>Cats</Menu.Item>
+            </Menu>
+          </Grid.Column>
+          <Grid.Column>
+            <Menu fluid vertical>
+              <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+              <Menu.Item className='header'>Cats</Menu.Item>
+            </Menu></Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
+  }
+}

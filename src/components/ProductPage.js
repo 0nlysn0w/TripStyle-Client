b93e986d609f092426e8abe6a548810bb32c1a10
Alexander <alexander_hockey@msn.com>
2018-12-12 14:40:33 +0100

    Products rendering on product page.

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -18,9 +18,16 @@ export class ProductPage extends Component {
     console.log(this.props.match.params.productid)
     axios.get('https://localhost:5001/api/product/' + product)
         .then(res=> {
-            console.log(res.data[0].name)
+            // console.log(res.data[0].name)
             this.setState({
-                product: res.data[0].name
+                product_name: res.data[0].name,
+                product_price: res.data[0].price,
+                product_image: res.data[0].images,
+                product_size: res.data[0].size,
+                product_make: res.data[0].make,
+                product_color: res.data[0].color,
+                product_region: res.data[0].region,
+                product_season: res.data[0].season,
             })
         })
 
@@ -42,21 +49,19 @@ export class ProductPage extends Component {
                     <GridColumn computer='8'>
                         <Divider hidden />
                         <Container textAlign='center'>
-                            <Image id="largeImage" src={'https://react.semantic-ui.com/images/wireframe/image.png'} size='big' />
+                            <Image id="largeImage" src={this.state.product_image} size='big' />
                         </Container>
                     </GridColumn>
                     <GridColumn width='5' verticalAlign='center'>
                         <Divider hidden />
-                        <Header size='huge' textAlign='right' color='red'> €20,- </Header>
+                        <Header size='huge' textAlign='right' color='red'> €{this.state.product_price},- </Header>
                         <Divider hidden/>
                         <Container textAlign='center'>
-                            <Header size='huge'>{this.state.product}</Header>
+                            <Header size='huge'>{this.state.product_name}</Header>
                             <Divider hidden/>
-                            Select size:
+                            Size:
                             <Container fluid>
-                            
-                            
-                            <SelectSize />
+                                <Header size='huge'> {this.state.product_size} </Header>
                             </Container>
                             <Divider hidden />
                             <Button color='green' size='massive' icon='shopping cart' fluid> 
@@ -69,9 +74,12 @@ export class ProductPage extends Component {
         <Divider hidden/>
         <Divider horizontal>Product information</Divider>
         <Container>
-        Quantum theory is the theoretical basis of modern physics that explains the nature and behavior of matter and energy on the atomic and subatomic level. The nature and behavior of matter and energy at that level is sometimes referred to as quantum physics and quantum mechanics.
-        In 1900, physicist Max Planck presented his quantum theory to the German Physical Society. Planck had sought to discover the reason that radiation from a glowing body changes in color from red, to orange, and, finally, to blue as its temperature rises. He found that by making the assumption that energy existed in individual units in the same way that matter does, rather than just as a constant electromagnetic wave - as had been formerly assumed - and was therefore quantifiable, he could find the answer to his question. The existence of these units became the first assumption of quantum theory.
-        Planck wrote a mathematical equation involving a figure to represent these individual units of energy, which he called quanta. The equation explained the phenomenon very well; Planck found that at certain discrete temperature levels (exact multiples of a basic minimum value), energy from a glowing body will occupy different areas of the color spectrum. Planck assumed there was a theory yet to emerge from the discovery of quanta, but, in fact, their very existence implied a completely new and fundamental understanding of the laws of nature. Planck won the Nobel Prize in Physics for his theory in 1918, but developments by various scientists over a thirty-year period all contributed to the modern understanding of quantum theory.
+            Name: {this.state.product_name}
+            Fabric: {this.state.product_make}
+            Color: {this.state.product_color}
+            Region: {this.state.product_region}
+            Season: {this.state.product_season}
+            Category: {this.state.product_category}
         </Container>
         <Divider hidden />
         <Footer />
